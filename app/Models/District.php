2a64088813a2608e3cfc6b6c3eb168d1c78ce12d
Ibrahim Toure <ibrahimtoure999999@gmail.com>
2026-06-04 16:43:38 +0000

    feat: ajout du modèle et de la migration pour les districts

---
+++ app/Models/District.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class District extends Model
+{
+    protected $fillable = [
+        'code_district',
+        'nom_district',
+        'annee',
+    ]
+
+    /**
+     * Définition de la relation : Un District possède plusieurs Régions.
+     * Cette méthode permettra d'écrire plus tard : District::query()->with('regions') pour éviter le problème de performance N+1.
+     *
+     * @return HasMany<Region, $this>
+     */
+
+    public function regions():HasMany
+    {
+        return $this->HasMany(Region::class, 'district_id', $this->getKeyName());
+    }
+}
---
+++ database/migrations/2026_06_04_161644_create_districts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Cette méthode s'exécute lorsque l'on lance la commande 'php artisan migrate'.
+     * Elle crée physiquement la table dans notre base de données.
+     */
+    public function up(): void
+    {
+        Schema::create('districts', function (Blueprint $table) {
+           
+            $table->bigIncrements('id');  // Crée une clé primaire auto-incrémentée
+            $table->string('code_district')->unique();
+            $table->string('nom_district');
+            $table->unsignedSmallInteger('annee')->nullable(); // Crée une colonne pour l'année de découpage ou décret, optionnelle (nullable) et stockée sous forme de petit entier positif
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('districts');
+    }
+};
